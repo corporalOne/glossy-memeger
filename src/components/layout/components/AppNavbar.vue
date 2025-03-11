6e21d6d24f5eac57dcfa9077a2af29d832f18e86
n.smirnov <tntall@bk.ru>
2025-03-11 18:06:09 +0300

    update templates

---
+++ src/components/layout/components/AppNavbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" aria-controls="mobile-menu" aria-expanded="false">
+                            <Bars3Icon
+                                @click="showSidebar = !showSidebar"
+                                class="block size-6"
+                            />
+                        </button>
+
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="hidden md:block">
+                        <div class="ml-4 flex items-center md:ml-6">
+                            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                <BellIcon class="size-6"/>
+                            </button>
+
+                            <div class="relative ml-3">
+                                <div>
+                                    <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                        <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <AppSidebar v-model="showSidebar" />
+        </nav>
+    </div>
+</template>
+<script setup lang="ts">
+import { Bars3Icon, BellIcon } from '@heroicons/vue/24/outline'
+import AppSidebar from '@/components/layout/components/AppSidebar.vue'
+import { ref } from 'vue';
+
+const showSidebar = ref<boolean>(false);
+</script>
---
+++ src/components/layout/components/AppSidebar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div
+        class="fixed top-0 left-0 flex w-64 h-full bg-gray-800 opacity-100"
+        :class="[ model ? 'flex-1 flex-col' : 'hidden' ]"
+    >
+        <div class="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
+            <nav class="flex-1 space-y-1 bg-gray-800 px-2" aria-label="Sidebar">
+                <a
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :href="item.href"
+                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
+                    <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                    <span class="flex-1">{{ item.name }}</span>
+                    <span v-if="item.count" :class="[item.current ? 'bg-gray-800' : 'bg-gray-900 group-hover:bg-gray-800', 'ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full']">{{ item.count }}</span>
+                </a>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from '@heroicons/vue/24/outline'
+
+const model = defineModel<boolean>({ required: true })
+
+const navigation = [
+    { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
+    { name: 'Team', icon: UsersIcon, href: '#', count: 3, current: false },
+    { name: 'Projects', icon: FolderIcon, href: '#', count: 4, current: false },
+    { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
+    { name: 'Documents', icon: InboxIcon, href: '#', current: false },
+    { name: 'Reports', icon: ChartBarIcon, href: '#', count: 12, current: false },
+]
+</script>
